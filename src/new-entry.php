f49e58a31322c7c2db8aa1ff1c3da03cdb716809
Ryan Rickgauer <rrickgauer1@gmail.com>
2019-12-08 01:21:27 -0600

    added required meta tags

--- src/new-entry.php
+++ src/new-entry.php
@@ -2,8 +2,8 @@
 <html lang="en" dir="ltr">
 
 <head>
-   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-   <meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta charset="utf-8" />
 
 	<title>New blog entry</title>
 </head>
