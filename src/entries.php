c274e22a1f56fb38d2cadaed4bc30a8f121b3e65
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-07-30 07:50:43 -0500

    rewrite webpage css

--- src/entries.php
+++ src/entries.php
@@ -1,169 +1,37 @@
 <?php
 include('functions.php');
 include('Parsedown.php');
+$entry = getEntry($_GET['entryID']);
+$Parsedown = new Parsedown();
 
-if (isset($_GET['entryID'])) {
-  $entry = getEntry($_GET['entryID']);
-}
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
 <head>
   <?php include('header.php'); ?>
-  <?php
-  if (isset($_GET['entryID'])) {
-    echo '<title>' . $entry['title'] . '</title>';
-  } else {
-    echo '<title>Ryan Rickgauer&apos;s Blog</title>';
-  }
-  ?>
-
+  <title><?php echo $entry['title']; ?></title>
 </head>
 
 <body>
-
-  <div class="wrapper">
-
-    <!-- sidebar  -->
-    <div id="sidebar" class="active">
-      <div class="sidebar-search">
-        <div class="input-group input-group-sm">
-          <input type="text" class="form-control" id="entry-search" aria-label="Search input" placeholder="Search" onkeyup="filterEntries()">
-          <div class="input-group-append" id="entry-search-icon">
-            <span class="input-group-text"><i class='bx bx-search'></i></span>
-          </div>
-        </div>
-      </div>
-
-      <!-- entries list -->
-      <ul class="list-unstyled" id="nav-list">
-        <?php
-        $entries = getAllEntries();
-        while ($entry = $entries->fetch(PDO::FETCH_ASSOC)) {
-          $id = $entry['id'];
-          $title = $entry['title'];
-          echo "<li class=\"sidebar-li\"><a class=\"sidebar-link\" href=\"entries.php?entryID=$id\">$title</a></li>";
-        }
-        ?>
-      </ul>
-    </div>
-
-
-
-    <div id="data" class="container-fluid">
-      <button id="show-entries" class="hamburger hamburger--elastic" type="button">
-        <span class="hamburger-box">
-          <span class="hamburger-inner"></span>
-        </span>
-      </button>
-
-      <script>
-        var $hamburger = $(".hamburger");
-        $hamburger.on("click", function(e) {
-          $hamburger.toggleClass("is-active");
-        });
-      </script>
-
-      <?php
-
-      if (isset($_GET['entryID'])) {
-        $entry = getEntry($_GET['entryID']);
-        echo '<h1 class="custom-font post-title">' . $entry['title'] . '</h1>';
-        echo '<h6 class="text-center entry-date">' . $entry['date'] . '</h6>';
-        $Parsedown = new Parsedown();
-        echo $Parsedown->text($entry['content']);
-      }
-
-      else {
-        include('home.php');
-      }
-
-      ?>
-
-      <script>
-        new TypeIt('.custom-font', {
-          speed: 50,
-          startDelay: 900
-        })
-        .options({speed: 50})
-
-        .go();
-      </script>
+    <div id="data" class="container">
+      <h1 class="custom-font post-title"><?php echo $entry['title']; ?></h1>
+      <h6 class="text-center entry-date"><?php echo $entry['date']; ?></h6>
+      <?php echo $Parsedown->text($entry['content']); ?>
 
       <div id="home-footer">
         <a href="https://github.com/rrickgauer/blog" target="_blank"><i class='bx bxl-github'></i></a>
       </div>
-
     </div>
-  </div>
 
   <script src="js/prism.js"></script>
-
   <script>
-    $(document).ready(function() {
-      // set all links to open in a new tab
-      // $("#data a").attr("target", "_blank");
-
-      $("#show-entries").on("click", function() {
-        $('#sidebar').toggleClass('active');
-        $('#data').toggleClass('active');
-
-        // clears search box and list all entries
-        $("#entry-search").val('');
-        filterEntries();
-      });
-    });
-
-
-
-    function filterEntries() {
-      // get search query
-      var filter = $("#entry-search").val().toUpperCase();
-      var ul = $("#nav-list");
-      var li = $(".sidebar-li");
-
-      if (filter.length == 0) {
-        setSearchIcon();
-      } else {
-        setClearButton();
-      }
-
-      // get list items
-      var a = $(".sidebar-link");
-
-      for (var count = 0; count < li.length; count++) {
-
-        a = li[count].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          li[count].style.display = "";
-        } else {
-          li[count].style.display = "none";
-        }
-      }
-    }
-
-    function setClearButton() {
-      $("#entry-search-icon").html('<button class="btn btn-outline-secondary" type="button" onclick="resetEntrySearch()"><i class="bx bx-x"></i></button>');
-    }
-
-    function setSearchIcon() {
-      $("#entry-search-icon").html('<span class="input-group-text"><i class="bx bx-search"></i></span>');
-    }
-
-    function resetEntrySearch() {
-      clearEntrySearch();
-      filterEntries();
-    }
-
-    function clearEntrySearch() {
-      $("#entry-search").val('');
-    }
+    new TypeIt('.custom-font', {
+      speed: 50,
+      startDelay: 900
+    })
+    .options({speed: 50})
+    .go();
   </script>
-
-
 </body>
-
 </html>
--- src/home.php
+++ src/home.php
@@ -1,17 +1,44 @@
-<div class="container">
-  <div class="vertical-center-page"></div>
-  <h1 id="hero"></h1>
+<?php include('functions.php'); ?>
+<?php include('Parsedown.php'); ?>
+<?php $entries = getAllEntries(); ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <?php include('header.php'); ?>
+  <title>Ryan Rickgauer's Blog</title>
+</head>
+<body>
+
+  <div class="container">
+
+    <h1 id="hero" class="custom-font mt-5 mb-5">Ryan Rickgauer's Blog</h1>
+
+    <ul class="list-group list-group-flush">
+      <?php
+        while ($entry = $entries->fetch(PDO::FETCH_ASSOC)) {
+          $id = $entry['id'];
+          $title = $entry['title'];
+          $date = $entry['date_formatted'];
+
+          echo '<li class="list-group-item entry">';
+          echo "<div  class=\"title\"><a href=\"entries.php?entryID=$id\">$title</a></div>";
+          echo "<div class=\"date\">$date</div>";
+          echo '</li>';
+        }
+      ?>
+    </ul>
+  </div>
+
+  
   <script>
-    $(document).ready(function() {
-      new TypeIt('#hero', {
-        speed: 90,
-        startDelay: 300
-      })
-      .options({speed: 50})
-      .type("Ryan Rickgauer's Blog")
-      .go();
-
-    });
+    new TypeIt('.custom-font', {
+      speed: 50,
+      startDelay: 900
+    })
+    .options({speed: 50})
+    .go();
   </script>
-</div>
+
+</body>
+</html>
