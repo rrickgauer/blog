e3d0f247b4dda21f17bc412e51fdcc05bba2cdef
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-09-15 23:24:40 -0500

    sort entries by title

--- src/js/home.js
+++ src/js/home.js
@@ -1,10 +1,22 @@
 
+const SORT_OPTIONS = {
+  DATE: "date",
+  TITLE: "title",
+};
+
+
 //////////
 // main //
 //////////
 $(document).ready(function() {
+  addEventListeners();
   initTypeIt();
 });
+
+
+function addEventListeners() {
+  $('.select-sort').on('change', sortEntries);
+}
 
 
 function initTypeIt() {
@@ -15,3 +27,31 @@ function initTypeIt() {
   .options({speed: 50})
   .go();
 }
+
+
+function sortEntries() {
+
+  var sortOption = $('.select-sort').val();
+
+  // if (sortOption = SORT_OPTIONS.DATE)
+  //   sortEntriesByDate();
+  // else
+  //   sortEntriesByTitle();
+
+  sortEntriesByTitle();
+}
+
+function sortEntriesByTitle() {
+  var entries = $('.entry');
+
+  entries.sort(function(a, b) {
+    var titleA = $(a).find('.title a').text().toUpperCase();
+    var titleB = $(b).find('.title a').text().toUpperCase();
+
+    return (titleA < titleB) ? -1 : 1;
+  });
+
+  $('.list-group').html(entries);
+}
+
+
