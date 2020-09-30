9d993ca5239421b9329cd922c630e6e72bac83c3
Ryan Rickgauer <rrickgauer1@gmail.com>
2020-09-30 18:58:42 -0500

    init admin

---
+++ admin/header.php
@@ -0,0 +1,14 @@
+<!-- bootstrap -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
+
+<!-- boxicons -->
+<link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet' />
+
+<!-- flatpickr: https://flatpickr.js.org/ -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css" />
+
+<!-- jquery toast: https://kamranahmed.info/toast -->
+<link rel="stylesheet" href="css/external/jquery.toast.min.css">
+
+<!-- personal css -->
+<link rel="stylesheet" type="text/css" href="css/style.css" />
