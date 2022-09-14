31242273c9c54f360dd8908cbecb26eeced5e4b1
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-13 19:40:01 -0500

    add conf files

--- scripts/dev/start-flask-server.bat
+++ scripts/dev/start-flask-server.bat
@@ -8,4 +8,4 @@ set flask_program=C:\xampp\htdocs\files\blog\src\.venv\Scripts\flask
 set FLASK_ENV=development
 set FLASK_APP=blog
 
-%flask_program% run --with-threads --host 0.0.0.0 --port 5020
+%flask_program% run --with-threads --host 0.0.0.0 --port 5050
