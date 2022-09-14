dce3e33917c8ba033a1b7173e4821c9b3a7f3534
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-09-13 20:24:50 -0500

    Update deploy.sh

--- scripts/vps/deploy.sh
+++ scripts/vps/deploy.sh
@@ -1,7 +1,10 @@
 
+cd "/var/www/blog"
 git pull
 
 # Refresh python dependencies
 ./var/www/blog/src/.venv/bin/pip install -r /var/www/blog/src/requirements.txt --upgrade
 
 
+cd "/var/www/blog/src/blog/static/css"
+sass style.scss style.css
