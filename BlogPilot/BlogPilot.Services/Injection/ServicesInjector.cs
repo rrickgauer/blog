1ab785342fd4ded54298d392bf166310f9443666
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-09-29 16:12:13 -0500

    setup dependency injection

---
+++ BlogPilot/BlogPilot.Services/Injection/ServicesInjector.cs
@@ -0,0 +1,48 @@
+﻿using BlogPilot.Services.Repository.Implementation;
+using BlogPilot.Services.Repository.Interface;
+using BlogPilot.Services.Service.Implementation;
+using BlogPilot.Services.Service.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPilot.Services.Injection;
+
+public class ServicesInjector
+{
+    public bool IsProduction { get; private set; }
+    public  IServiceCollection Services { get; private set; }
+
+    public ServicesInjector(IServiceCollection services, bool isProduction)
+    {
+        Services = services;
+        IsProduction = isProduction;
+    }
+
+
+    public void InjectDependencies()
+    {
+        //// set the appropriate configuration class
+        //// depends if the app is running in development or production
+        //if (isDevlopment)
+        //{
+        //    services.AddSingleton<IConfigs, ConfigurationDev>();
+        //}
+        //else
+        //{
+        //    services.AddSingleton<IConfigs, ConfigurationProduction>();
+        //}
+
+
+        // services
+        Services.AddSingleton<IModelMapperService, ModelMapperService>();
+        Services.AddSingleton<IEntryService, EntryService>();
+
+        // repositories
+        Services.AddSingleton<IEntryRepository, EntryRepository>();
+
+    }
+}
--- BlogPilot/BlogPilot/Program.cs
+++ BlogPilot/BlogPilot/Program.cs
@@ -1,6 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BlogPilot;
+using BlogPilot.Services.Injection;
+using BlogPilot.Services.Service.Interface;
+using Microsoft.Extensions.DependencyInjection;
 
 
+bool isProduction = true;
 
-    
+#if DEBUG
+isProduction = false;
+#endif
+
+ServiceCollection services = new();
+var injector = new ServicesInjector(services, isProduction);
+injector.InjectDependencies();
+
+ServiceProvider serviceProvider = services.BuildServiceProvider();
+
+var entryService = serviceProvider.GetRequiredService<IEntryService>();
+entryService.Test();
+
+int x = 10;
+
+
