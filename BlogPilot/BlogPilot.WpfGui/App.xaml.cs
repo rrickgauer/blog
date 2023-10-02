aaf3d2a1855b5facaba0c3b7927837022deb1da8
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-10-02 12:37:52 -0500

    add the wpf ui project

---
+++ BlogPilot/BlogPilot.WpfGui/App.xaml.cs
@@ -0,0 +1,138 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using BlogPilot.Services.Injection;
+using BlogPilot.WpfGui.Models;
+using BlogPilot.WpfGui.Services;
+using BlogPilot.WpfGui.ViewModels.Pages;
+using BlogPilot.WpfGui.ViewModels.Windows;
+using BlogPilot.WpfGui.Views.Pages;
+using BlogPilot.WpfGui.Views.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Threading;
+using Wpf.Ui.Mvvm.Contracts;
+using Wpf.Ui.Mvvm.Services;
+
+namespace BlogPilot.WpfGui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        // The.NET Generic Host provides dependency injection, configuration, logging, and other services.
+        // https://docs.microsoft.com/dotnet/core/extensions/generic-host
+        // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
+        // https://docs.microsoft.com/dotnet/core/extensions/configuration
+        // https://docs.microsoft.com/dotnet/core/extensions/logging
+        private static readonly IHost _host = Host
+            .CreateDefaultBuilder()
+            .ConfigureAppConfiguration(c => { c.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)); })
+            .ConfigureServices((context, services) =>
+            {
+                #region - WPF UI -
+                // inject the WPF UI services
+                InjectWpfUIServices(services);
+
+                // Configuration
+                services.Configure<AppConfig>(context.Configuration.GetSection(nameof(AppConfig)));
+
+                #endregion
+
+
+                // inject all the required logic/db services
+                bool isProduction = true;
+
+                #if DEBUG
+                isProduction = false;
+                #endif
+
+                ServicesInjector servicesInjector = new(services, isProduction);
+                servicesInjector.InjectDependencies();
+
+                // inject all the custom view/view models
+                //services.AddSingleton<CustomAlertsService>();
+
+                services.AddScoped<LandingViewModel>();
+                services.AddScoped<LandingPage>();
+
+
+            }).Build();
+
+
+        /// <summary>
+        /// Inject all the services required for WPF UI
+        /// </summary>
+        /// <param name="services"></param>
+        private static void InjectWpfUIServices(IServiceCollection services)
+        {
+            // App Host
+            services.AddHostedService<ApplicationHostService>();
+
+            services.AddSingleton<ISnackbarService, SnackbarService>();
+
+            // Page resolver service
+            services.AddSingleton<IPageService, PageService>();
+
+            // Theme manipulation
+            services.AddSingleton<IThemeService, ThemeService>();
+
+            // TaskBar manipulation
+            services.AddSingleton<ITaskBarService, TaskBarService>();
+
+            // Service containing navigation, same as INavigationWindow... but without window
+            services.AddSingleton<INavigationService, NavigationService>();
+
+            // Main window with navigation
+            services.AddScoped<INavigationWindow, MainWindow>();
+            services.AddScoped<MainWindowViewModel>();
+
+            services.AddScoped<SettingsPage>();
+            services.AddScoped<SettingsViewModel>();
+        }
+
+        /// <summary>
+        /// Gets registered service.
+        /// </summary>
+        /// <typeparam name="T">Type of the service to get.</typeparam>
+        /// <returns>Instance of the service or <see langword="null"/>.</returns>
+        public static T GetService<T>()
+            where T : class
+        {
+            return _host.Services.GetService(typeof(T)) as T;
+        }
+
+        /// <summary>
+        /// Occurs when the application is loading.
+        /// </summary>
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            _host.Start();
+        }
+
+        /// <summary>
+        /// Occurs when the application is closing.
+        /// </summary>
+        private async void OnExit(object sender, ExitEventArgs e)
+        {
+            await _host.StopAsync();
+
+            _host.Dispose();
+        }
+
+        /// <summary>
+        /// Occurs when an exception is thrown by an application but not handled.
+        /// </summary>
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // For more info see https://docs.microsoft.com/en-us/dotnet/api/system.windows.application.dispatcherunhandledexception?view=windowsdesktop-6.0
+        }
+    }
+}
