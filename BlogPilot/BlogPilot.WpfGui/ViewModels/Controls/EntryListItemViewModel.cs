e939a108d614ba931491af9c2d40c7b6c69c76ff
Ryan Rickgauer <rrickgauer1@gmail.com>
2023-10-02 19:39:17 -0500

    remove unused imports

--- BlogPilot/BlogPilot.WpfGui/ViewModels/Controls/EntryListItemViewModel.cs
+++ BlogPilot/BlogPilot.WpfGui/ViewModels/Controls/EntryListItemViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using BlogPilot.Services.Domain.TableViews;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BlogPilot.WpfGui.ViewModels.Controls;
 
