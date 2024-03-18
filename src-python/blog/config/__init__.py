4f0545cfbcc37b5d3da9afed3ab2638388760150
Ryan Rickgauer <rrickgauer1@gmail.com>
2024-03-18 18:46:23 -0500

    add new project

--- src-python/blog/config/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import ConfigBase as ConfigBase
-from .configs import ConfigProduction as ConfigProduction
-from .configs import ConfigDev as ConfigDev
---
+++ src/gui/Blog/Blog.WpfGui/ViewModels/Pages/SettingsViewModel.cs
@@ -0,0 +1,68 @@
+﻿// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+
+namespace Blog.WpfGui.ViewModels.Pages
+{
+    public partial class SettingsViewModel : ObservableObject, INavigationAware
+    {
+        private bool _isInitialized = false;
+
+        [ObservableProperty]
+        private string _appVersion = String.Empty;
+
+        [ObservableProperty]
+        private ApplicationTheme _currentTheme = ApplicationTheme.Unknown;
+
+        public void OnNavigatedTo()
+        {
+            if (!_isInitialized)
+                InitializeViewModel();
+        }
+
+        public void OnNavigatedFrom() { }
+
+        private void InitializeViewModel()
+        {
+            CurrentTheme = ApplicationThemeManager.GetAppTheme();
+            AppVersion = $"UiDesktopApp1 - {GetAssemblyVersion()}";
+
+            _isInitialized = true;
+        }
+
+        private string GetAssemblyVersion()
+        {
+            return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+                ?? String.Empty;
+        }
+
+        [RelayCommand]
+        private void OnChangeTheme(string parameter)
+        {
+            switch (parameter)
+            {
+                case "theme_light":
+                    if (CurrentTheme == ApplicationTheme.Light)
+                        break;
+
+                    ApplicationThemeManager.Apply(ApplicationTheme.Light);
+                    CurrentTheme = ApplicationTheme.Light;
+
+                    break;
+
+                default:
+                    if (CurrentTheme == ApplicationTheme.Dark)
+                        break;
+
+                    ApplicationThemeManager.Apply(ApplicationTheme.Dark);
+                    CurrentTheme = ApplicationTheme.Dark;
+
+                    break;
+            }
+        }
+    }
+}
